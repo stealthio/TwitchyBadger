656419286d540d016e615b9cdc9b914ef84f9daa
Paul T <20127926+stealthio@users.noreply.github.com>
2019-11-07 14:38:32 +0100

    Implemented color replacing and cropping

---
+++ Cropable.gd
@@ -0,0 +1,31 @@
+extends TextureRect
+
+var rectangle = Rect2(0,0,15,15)
+var drawing = false
+
+signal on_crop(tex)
+
+func _input(event):
+	if event is InputEventMouseButton:
+		var m = get_global_mouse_position()
+		if m.x > rect_global_position.x and m.y > rect_global_position.y and m.x < rect_global_position.x + rect_size.x and m.y < rect_global_position.y + rect_size.y:
+			drawing = !drawing
+			if drawing:
+				rectangle = Rect2(get_global_mouse_position() - rect_global_position, Vector2(1,1))
+				update()
+	if event is InputEventMouseMotion:
+		if drawing:
+			rectangle.size = (get_global_mouse_position() - rect_global_position) - rectangle.position
+			update()
+
+func _draw():
+	draw_rect(rectangle, Color.antiquewhite, false)
+
+func _on_Crop_pressed():
+	var data : Image = texture.get_data()
+	var x = data.get_rect(rectangle)
+	var imageTexture = ImageTexture.new()
+	imageTexture.create_from_image(x)
+	rectangle = Rect2(0,0,0,0)
+	update()
+	emit_signal("on_crop", imageTexture.duplicate(true))
--- Main.gd
+++ Main.gd
@@ -2,8 +2,12 @@ extends Node2D
 
 onready var fileDialogImport = $UI/FileDialogImport
 onready var fileDialogExport = $UI/FileDialogExport
-onready var previews = [$UI/VBoxContainer/Preview, $UI/BackgroundEmote/Preview28, $UI/BackgroundEmote/Preview56, $UI/BackgroundEmote/Preview112, $UI/BackgroundBadge/Preview18, $UI/BackgroundBadge/Preview36, $UI/BackgroundBadge/Preview72]
+onready var importPreview = $UI/VBoxContainer/Preview
+onready var previews = [$UI/BackgroundEmote/Preview28, $UI/BackgroundEmote/Preview56, $UI/BackgroundEmote/Preview112, $UI/BackgroundBadge/Preview18, $UI/BackgroundBadge/Preview36, $UI/BackgroundBadge/Preview72, $UI/BackgroundEdit/TextureRect]
 onready var scalingMethod = $UI/OptionButton
+onready var colorFrom = $UI/BackgroundEdit/BtnReplaceColor/ColorFrom
+onready var colorTo = $UI/BackgroundEdit/BtnReplaceColor/ColorTo
+onready var cropper = $UI/BackgroundEdit/TextureRect
 
 var tex : ImageTexture = null
 var format = 0 # 0 = emote, 1 = badge
@@ -15,12 +19,13 @@ func _ready():
 	scalingMethod.add_item("Bilinear")
 	scalingMethod.add_item("Cubic")
 	scalingMethod.add_item("Trilinear")
+	cropper.connect("on_crop", self, "_on_crop")
 
 func import_file_selected(path):
 	tex = ImageTexture.new()
 	tex.load(path)
-	for preview in previews:
-		preview.texture = tex
+	importPreview.texture = tex.duplicate(true)
+	_refresh_previews()
 
 func export_file_selected(path):
 	if tex != null:
@@ -46,3 +51,23 @@ func _on_ButtonExportEmote_pressed():
 func _on_ButtonExportBadge_pressed():
 	fileDialogExport.popup(Rect2(0,0,420,300))
 	format = 1
+
+func _refresh_previews():
+	for preview in previews:
+		preview.texture = tex
+
+func _on_crop(newTex):
+	tex = newTex
+	_refresh_previews()
+
+func _on_ButtonReplaceColor_pressed():
+	var img : Image = tex.get_data()
+	img.lock()
+	for y in img.get_height():
+		for x in img.get_width():
+			var px = img.get_pixel(x, y)
+			if px == colorFrom.color:
+				img.set_pixel(x,y, colorTo.color)
+	img.unlock()
+	tex.set_data(img)
+		
